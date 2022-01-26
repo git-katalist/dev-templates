514f544f5118a52061a0f67c06f664f1b8c22487
tsots <mosalatsotetsi@gmail.com>
2022-01-26 03:52:25 +0200

    initial files for setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components";
+
+import { Theme } from "../styles/Theme";
+import { Global } from "styles/Global";
+import Topbar from "@Components/topbar/Topbar";
+import Footer from "@Components/footer/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={Theme}>
+      <Global />
+      <Topbar />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
